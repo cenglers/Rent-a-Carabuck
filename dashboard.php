0429322ce1db748f0430e34f170dd48f39766f19
rdvnrslnn <147540978+rdvnrslnn@users.noreply.github.com>
2025-05-12 22:14:40 +0300

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -4,16 +4,21 @@ include("includes/db.php");
 
 $result = $conn->query("SELECT * FROM cars WHERE available = 1");
 
-echo "<h2>Available Cars:</h2><ul>";
+echo "<h2>Available Cars</h2><ul>";
 while ($row = $result->fetch_assoc()) {
   echo "<li>{$row['model']} - <button onclick='bookCar({$row['id']})'>Book</button></li>";
 }
+echo "</ul>";
 ?>
+
 <div id="result"></div>
 <script>
 function bookCar(id) {
   fetch("ajax/book_car.php?id=" + id)
     .then(res => res.text())
-    .then(data => document.getElementById("result").innerHTML = data);
+    .then(data => {
+      document.getElementById("result").innerHTML = data;
+      setTimeout(() => location.reload(), 1000);
+    });
 }
 </script>
