b19659117399e2f1d3527e8d5763715a3096e5e6
1283ys <146979212+1283ys@users.noreply.github.com>
2025-05-12 18:10:18 +0300

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,19 @@
+<?php
+include("includes/auth.php");
+include("includes/db.php");
+
+$result = $conn->query("SELECT * FROM cars WHERE available = 1");
+
+echo "<h2>Available Cars:</h2><ul>";
+while ($row = $result->fetch_assoc()) {
+  echo "<li>{$row['model']} - <button onclick='bookCar({$row['id']})'>Book</button></li>";
+}
+?>
+<div id="result"></div>
+<script>
+function bookCar(id) {
+  fetch("ajax/book_car.php?id=" + id)
+    .then(res => res.text())
+    .then(data => document.getElementById("result").innerHTML = data);
+}
+</script>
